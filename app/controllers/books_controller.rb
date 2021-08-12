414ad32d1379f54efc0ad9994e7c6cb1ce09f0ea
ir060906@gmail.com <ec2-user@ip-172-31-22-150.ec2.internal>
2021-08-12 15:00:25 +0000

    [add]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
-end
+  def top
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to books_path
+    else
+    render :new
+    end  
+  end
+  
+  def edit
+    @books = Book.all
+  end
+
+  def destroy
+    @books = Book.all
+    book.destroy
+    redirect_to books_path
+  end
+
+  end
