9ede2fdf649acbed957fc1dc8f99caf61b6a49bf
ir060906@gmail.com <ec2-user@ip-172-31-22-150.ec2.internal>
2021-08-15 05:52:12 +0000

    [add]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,28 +8,27 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books = Book.new
+    @book = Book.find(params[:id])
   end
 
   def new
-    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(@books)
-    
+    redirect_to book_path(book[:id])
+
   end
 
   def edit
-    @books = Book.all
+    @book = Book.find(params[:id])
   end
 
   def destroy
     @book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path(@book.id)
+    @book.destroy
+    redirect_to books_path
   end
 
 private
