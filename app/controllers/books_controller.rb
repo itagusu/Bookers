03ce59fbfdc9a972517401462fd07bbfca097cbe
ir060906@gmail.com <ec2-user@ip-172-31-22-150.ec2.internal>
2021-08-16 06:45:13 +0000

    [finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,21 +17,27 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-    render :index
+      @books = Book.all
+      render :index
     end
-
   end
 
   def edit
     @book = Book.find(params[:id])
+    
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
